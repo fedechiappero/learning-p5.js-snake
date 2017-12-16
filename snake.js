df9e1e86832768ee4e800c689a3522c5c4b5f57d
Federico Bit <fedechiappero06@gmail.com>
2017-12-16 18:37:47 -0300

    original project

---
+++ snake.js
@@ -0,0 +1,58 @@
+function Snake(){
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.eat = function(pos){//if dist<1 then eat
+    var d = dist(this.x, this.y, pos.x, pos.y)
+    if (d < 1){
+      this.total++;//increase the tail
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  this.death = function(){
+    for(var i = 0; i < this.tail.length; i++){
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1){
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
+
+  this.dir = function(x, y){
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  this.update = function(){
+    for(var i = 0; i < this.tail.length - 1; i++){
+      this.tail[i] = this.tail[i + 1];
+    }
+    if (this.total >= 1){
+      this.tail[this.total - 1] = createVector(this.x, this.y);
+    }
+  
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
+
+    //constrain the snake inside the frame
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
+  }
+
+  this.show = function (){
+    fill(255);
+    for(var i = 0; i < this.tail.length; i++){
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+  }
+}
